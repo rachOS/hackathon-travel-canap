f2d79e2c77f66bc3ece157ec92d9e0d374ff1b65
rachOS <g.chamekh@e-nautia.com>
2020-04-29 20:35:46 +0200

    dev header/footer and css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './footer.css'
 
 export default function Footer(){
     return(
-        <div>Footer</div>
+        <div className="footer">
+            <p className="credit">
+                website crafted with gluttony by <a className="link" href="https://github.com/Jwana24">Johanna</a>, <a className="link" href="https://github.com/mathildetho">Mathilde</a> and <a className="link" href="https://github.com/rachOS">Grégory</a>
+            </p>
+        </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import './header.css'
+import logo from '../img/treap-logo.png'
 
 export default function Header(){
     return(
-        <div>Header</div>
+        <div className="header">
+            <img className="logo" src={logo} alt="logo Treap" title="logo-treap"/>
+        </div>
     )
 }
