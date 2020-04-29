c05823e4d97b07f53ec0e4594298607775bfa1e8
mathildetho <mathilde.thoraninth@gmail.com>
2020-04-29 21:54:43 +0200

    container meal create

--- src/components/TreapContainer.js
+++ src/components/TreapContainer.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Destination from './Destination';
+import ContainerMeal from './ContainerMeal';
 
 export default function TreapContainer(){
+    const [mealRandom,setMeals] = useState({});
+    useEffect(()=> {
+        changeMeal();
+    }, [])
+
+    const changeMeal = () => {
+        axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(res=> setMeals(res.data.meals[0]))
+    }
+
     return(
         <div>
-            <Destination />
+            {/*<Destination />*/}
+            <ContainerMeal 
+                mealRandom={mealRandom}
+                changeMeal={changeMeal}
+            />
         </div>
     )
 }
