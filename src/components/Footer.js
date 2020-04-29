eb30022b0e24f1b51518ae6e14a8baab8781b52c
rachOS <g.chamekh@e-nautia.com>
2020-04-29 22:09:26 +0200

    add target='blank' attribut on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(){
     return(
         <div className="footer">
             <p className="credit">
-                website crafted with gluttony by <a className="link" href="https://github.com/Jwana24">Johanna</a>, <a className="link" href="https://github.com/mathildetho">Mathilde</a> and <a className="link" href="https://github.com/rachOS">Grégory</a>
+                website crafted with gluttony by <a target="blank" className="link" href="https://github.com/Jwana24">Johanna</a>, <a target="blank" className="link" href="https://github.com/mathildetho">Mathilde</a> and <a target="blank" className="link" href="https://github.com/rachOS">Grégory</a>
             </p>
         </div>
     )
