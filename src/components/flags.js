b02eb54f2b948a8d6722aa9de94ec75ec91a072c
Jwana24 <detrieux.btscom13@gmail.com>
2020-04-30 05:25:08 +0200

    create sidebar flags, updates css

--- src/components/flags.js
+++ src/components/flags.js
@@ -1,12 +1,21 @@
 import French from "../img/france.png";
+import Greek from "../img/greece.png";
 import Spanish from "../img/spain.png";
 import English from "../img/england.png";
+import Canadian from "../img/canada.png";
+import American from "../img/usa.png";
 import Japanese from "../img/japan.png";
+import Chinese from "../img/china.png";
+import Moroccan from "../img/moroccan.png";
 
 const flags = [
     {
         img: French,
         name: 'French'
+    },
+    {
+        img: Greek,
+        name: 'Greek'
     },
     {
         img: Spanish,
@@ -17,8 +26,24 @@ const flags = [
         name:'British'
     },
     {
+        img: Canadian,
+        name:'Canadian'
+    },
+    {
+        img: American,
+        name:'American'
+    },
+    {
         img: Japanese,
         name: "Japanese"
+    },
+    {
+        img: Chinese,
+        name: "Chinese"
+    },
+    {
+        img: Moroccan,
+        name: "Moroccan"
     },
 ]
 
